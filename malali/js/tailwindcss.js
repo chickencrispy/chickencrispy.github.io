4634ab56e5dd7adf10db41cb077d0976b775a5e1
Chicken Crispy <debi.rahmadi@gmail.com>
2023-12-10 23:45:11 +0700

    malali.

---
+++ malali/js/tailwindcss.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+
+      }
+    },
+    fontFamily: {
+      'poppins': ['Poppins','Sans-serif']
+    },
+    fontSize: {
+      sm    : '.7rem',
+      base  : '.8rem',
+      lg    : '1rem',
+      xl    : '1.2rem',
+      '2xl' : '1.5rem'
+    }
+  }
+}
