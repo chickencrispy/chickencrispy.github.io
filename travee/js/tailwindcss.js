34bbe473796402913d60ec584ddb364db878da7e
Chicken Crispy <debi.rahmadi@gmail.com>
2023-12-17 18:15:47 +0700

    travee.

---
+++ travee/js/tailwindcss.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      flex: {
+        '0' : '0 0 auto'
+      }
+    },
+    fontFamily: {
+      'poppins': ['Poppins','Sans-serif'],
+      'barlow': ['Barlow', 'Sans-serif']
+    },
+    fontSize: {
+      sm    : '.75rem',
+      base  : '.9rem',
+      lg    : '1rem',
+      xl    : '1.2rem',
+      '2xl' : '1.5rem'
+    }
+  }
+}
---
+++ travee/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      flex: {
+        '0': '0 0 auto'
+      }
+    },
+    fontFamily: {
+      'poppins': ['Poppins', 'Sans-serif'],
+      'barlow' : ['Barlow', 'Sans-serif']
+    },
+    fontSize: {
+      sm   : '.75rem',
+      base : '.9rem',
+      lg   : '1rem',
+      xl   : '1.2rem',
+      '2xl': '1.5rem'
+    }
+  },
+  plugins: [],
+}
