a314e45fc9e4482140bc9dc5e5be8511d54827a9
Aladinakapsul <strengeract@gmail.com>
2024-05-24 22:41:09 +0700

    update

---
+++ banyumilir/backend/mysql_connector.php
@@ -0,0 +1,16 @@
+<?php
+
+$servertype="mysql";
+$serverhost="103.246.3.4";
+$dbname="ticketing";
+$dbuser="root174";
+$dbpassword="root";
+
+$con = mysqli_connect($serverhost,$dbuser,$dbpassword,$dbname);
+if(mysqli_connect_errno()){
+    echo "Failed to connect to Mysql:".mysqli_connect_errno();
+    exit();
+}
+
+
+?>
