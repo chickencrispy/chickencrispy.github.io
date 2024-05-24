9613c2efbf16f2f07040a1d53429b771726ec5be
Aladinakapsul <strengeract@gmail.com>
2024-05-24 23:52:14 +0700

    update

--- banyumilir/backend/front_be.php
+++ banyumilir/backend/front_be.php
@@ -6,25 +6,12 @@ date_default_timezone_set('Asia/Jakarta');
 require('mysql_connector.php');
 
 
-//*****************************GET DATA CFG_PACKET ALL GROUP***************************************************
-$result_front_be_cfg_packet = mysqli_query($con, "SELECT * FROM ticketing.01_cfg_packet where company_id like '1' order by index_category asc;");
-while ($row_front_be_cfg_packet = mysqli_fetch_row($result_front_be_cfg_packet)){
+$cat = $_REQUEST['cat'] ?? "";
 
-  //*****************************PER GROUP***************************************************
-  $result_group_be_cfg_packet = mysqli_query($con, "SELECT * FROM ticketing.01_cfg_packet where company_id like '1' and index_category like '".$row_front_be_cfg_packet[10]."';");
-  //$row_group_be_cfg_packet = mysqli_fetch_row($result_group_be_cfg_packet);
+//*****************************GET DATA CFG_PACKET***************************************************
+$result_front_be_cfg_packet = mysqli_query($con, "SELECT * FROM ticketing.01_cfg_packet where company_id like '1' and category_packet like '".$cat."';");
+//$row_front_be_cfg_packet = mysqli_fetch_row($result_front_be_cfg_packet);
 
-    if ($result_group_be_cfg_packet->num_rows > 0) {
-      while($row_group_be_cfg_packet = $result_group_be_cfg_packet->fetch_assoc()) {
-          $data_cfg_packet[] = $row_group_be_cfg_packet;
-      }
-    } else {
-      echo "0 results";
-    }
-
-}
-
-/*
 if ($result_front_be_cfg_packet->num_rows > 0) {
     while($row_front_be_cfg_packet = $result_front_be_cfg_packet->fetch_assoc()) {
         $data_cfg_packet[] = $row_front_be_cfg_packet;
@@ -32,7 +19,7 @@ if ($result_front_be_cfg_packet->num_rows > 0) {
   } else {
     echo "0 results";
   }
-*/
+
 
 //print"<pre>";print_r($data_cfg_packet);print"</pre>";
 
