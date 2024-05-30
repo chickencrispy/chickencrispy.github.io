05ce380e0f2e1520f95426e50201310cb37fa6ac
chickencrispy <debi.rahmadi@gmail.com>
2024-05-30 22:18:20 +0700

    add set default timezone

--- banyumilir/backend/mysql_connector.php
+++ banyumilir/backend/mysql_connector.php
@@ -1,16 +1,15 @@
 <?php
+  date_default_timezone_set('Asia/Jakarta');
+  
+  $servertype="mysql";
+  $serverhost="103.246.3.2";
+  $dbname="ticketing";
+  $dbuser="root174";
+  $dbpassword="root";
 
-$servertype="mysql";
-$serverhost="103.246.3.2";
-$dbname="ticketing";
-$dbuser="root174";
-$dbpassword="root";
-
-$con = mysqli_connect($serverhost,$dbuser,$dbpassword,$dbname);
-if(mysqli_connect_errno()){
+  $con = mysqli_connect($serverhost,$dbuser,$dbpassword,$dbname);
+  if(mysqli_connect_errno()){
     echo "Failed to connect to Mysql:".mysqli_connect_errno();
     exit();
-}
-
-
+  }
 ?>
