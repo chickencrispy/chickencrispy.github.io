c4333f49357aaeebbbdeaa29462624558e89be5a
Aladinakapsul <strengeract@gmail.com>
2024-06-07 00:18:23 +0700

    update

--- banyumilir/submit.php
+++ banyumilir/submit.php
@@ -1,9 +1,17 @@
-<?php
+<script>
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+} else {
+    console.log("Geolocation is not supported by this browser.");
+}
+
+function showPosition(position) {
+    console.log("Latitude: " + position.coords.latitude + 
+    " Longitude: " + position.coords.longitude);
+    // Di sini Anda bisa melakukan sesuatu dengan data lokasi yang diperoleh, seperti menampilkan peta atau mengirimkan data ke server.
+}
 
 
-print $_REQUEST['signature'];
 
-
-?>
-
-
+</script>
